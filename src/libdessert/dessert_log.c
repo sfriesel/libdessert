cc584d4e30083c887056719cebf63c0c8b9e2ae8
blywis <blywis@ec5dfc5b-5239-0410-a031-e37cc464ff32>
2011-03-06 17:10:59 +0000

    fixed bug: log flush task did not return 0
    git-svn-id: https://svn.imp.fu-berlin.de/des-testbed/Software/DES-SERT/libdessert/trunk@7078 ec5dfc5b-5239-0410-a031-e37cc464ff32

--- src/libdessert/dessert_log.c
+++ src/libdessert/dessert_log.c
@@ -285,6 +285,8 @@ void _dessert_log(int level, const char* func, const char* file, int line, const
  * @param data ignored
  * @param scheduled time when the callback should have been called
  * @param interval ignored
+ *
+ * @return 0 (do not unregister)
  */
 int dessert_flush_log(void *data, struct timeval *scheduled, struct timeval *interval) {
     if(dessert_logfd != NULL) {
@@ -303,6 +305,7 @@ int dessert_flush_log(void *data, struct timeval *scheduled, struct timeval *int
     }
 #endif
     dessert_debug("*** flushed log ***");
+    return 0;
 }
 
 /**
